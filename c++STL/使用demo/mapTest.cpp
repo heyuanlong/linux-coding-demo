38084ab0e5704bbb753e84a78fedf3aaa1401d4b
heyuanlong <1023417614@qq.com>
2016-11-23 17:26:31 +0800

    sfdsklfsjdf

---
+++ c++STL/使用demo/mapTest.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <map>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	//增
+
+	//删
+
+	//改
+
+	//查
+	
+
+
+	
+	return 0;
+}
---
+++ c++STL/使用demo/vectorTest.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	//增
+
+	//删
+
+	//改
+
+	//查
+	
+
+
+
+
+	
+	return 0;
+}
